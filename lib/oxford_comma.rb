dd8e6370736ddb3e6b3250e09b54370cb00a1632
John Raber <jrab224@gmail.com>
2018-06-02 18:46:23 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,13 @@
 def oxford_comma(array)
   if array.size == 1
     array[0]
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    index = 0
+    list = ""
+    until index == array.size - 1
+      array.join(", ")
+    end
   end
 end
